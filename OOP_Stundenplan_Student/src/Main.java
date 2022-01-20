bfb5cba914e58abc0c86e29bb7867e0cb8ba8843
IkeTurtle <97555754+IkeTurtle@users.noreply.github.com>
2022-01-20 12:32:00 +0100

    First Commit

---
+++ OOP_Stundenplan_Student/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+					
+		CourseDescriptionPanel descriptionPanel = new CourseDescriptionPanel();
+		StudCourseListPanel courseListPanel = new StudCourseListPanel();
+		
+	}
+
+}
