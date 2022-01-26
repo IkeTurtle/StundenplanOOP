d2b4db3fa4f423afa7a139077fcd880aec2b0c79
quang55 <97985075+quang55@users.noreply.github.com>
2022-01-26 12:45:48 +0100

    Update LectCourseCreate.java

--- OOPStundenplanJRE15/src/LectCourseCreate.java
+++ OOPStundenplanJRE15/src/LectCourseCreate.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,6 +11,7 @@ import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 public class LectCourseCreate extends JPanel {
+
 
 	public LectCourseCreate() {
 		
@@ -43,17 +45,29 @@ public class LectCourseCreate extends JPanel {
 		name.setHorizontalAlignment(SwingConstants.CENTER);
 		name.setVerticalAlignment(SwingConstants.CENTER);
 		
-		name.setBounds(175,50,150,40);
+		name.setBounds(12,50,150,40);
 		
 		myPanel.add(name);
 		
 
 		JTextArea namebox = new JTextArea();
-		namebox.setBounds(70,90,360,20);
+		namebox.setBounds(70,90,230,20);
 		myPanel.add(namebox);
+
 		
+		JLabel abkuerzung = new JLabel("Abkürzung");
 		
+		abkuerzung.setHorizontalAlignment(SwingConstants.CENTER);
+		abkuerzung.setVerticalAlignment(SwingConstants.CENTER);
 		
+		abkuerzung.setBounds(286,50,150,40);
+		
+		myPanel.add(abkuerzung);
+		
+
+		JTextArea abkuerzungbox = new JTextArea();
+		abkuerzungbox.setBounds(330,90,100,20);
+		myPanel.add(abkuerzungbox);
 		
 		JLabel beschreibung = new JLabel("Beschreibung");
 		
@@ -80,18 +94,66 @@ public class LectCourseCreate extends JPanel {
 		
 		myPanel.add(zeit);
 		
-		JTextArea zeitbox = new JTextArea();
-		zeitbox.setBounds(70,360,360,100);
-		myPanel.add(zeitbox);
 		
+		
+		JLabel tag = new JLabel("Wochentag");
+		
+		tag.setHorizontalAlignment(SwingConstants.CENTER);
+		tag.setVerticalAlignment(SwingConstants.CENTER);
+		
+		tag.setBounds(52,360,100,40);
+		
+		myPanel.add(tag);
+		
+		String days[] = {"Montag","Dienstag","Mittwoch","Donnerstag","Freitag"};
+		JComboBox <String> day = new JComboBox<String>(days);
+		
+		day.setBounds(70,400,120,20);
+		myPanel.add(day);
+		
+		
+		
+		JLabel start = new JLabel("Von");
+		
+		start.setHorizontalAlignment(SwingConstants.CENTER);
+		start.setVerticalAlignment(SwingConstants.CENTER);
+		
+		start.setBounds(226,360,50,40);
+		
+		myPanel.add(start);
+		
+		String time1[] = {"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00"};
+		JComboBox <String> starttime = new JComboBox<String>(time1);
+		
+		starttime.setBounds(240,400,70,20);
+		myPanel.add(starttime);
+		
+		
+		JLabel stop = new JLabel("Bis");
+		
+		stop.setHorizontalAlignment(SwingConstants.CENTER);
+		stop.setVerticalAlignment(SwingConstants.CENTER);
+		
+		stop.setBounds(344,360,50,40);
+		
+		myPanel.add(stop);
+		
+		String time2[] = {"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00"};
+		JComboBox <String> stoptime = new JComboBox<String>(time2);
+		
+		stoptime.setBounds(360,400,70,20);
+		myPanel.add(stoptime);
 		
 		JButton create = new JButton("Fertig");
 		
 		create.setBounds(325, 495, 100, 30);
+		
+		create.addActionListener(new LectCourseCreateListener(namebox,abkuerzungbox,beschreibungbox, day, starttime,stoptime));
+		
 		myPanel.add(create);
 		
 		
-		JButton zurueck = new JButton("ZurÃ¼ck");
+		JButton zurueck = new JButton("Zurück");
 		
 		zurueck.setBounds(75, 495, 100, 30);
 		myPanel.add(zurueck);
@@ -103,13 +165,10 @@ public class LectCourseCreate extends JPanel {
 				
 				LectFun frame = new LectFun();	
 				myFrame.dispose();
-			}		
-			
-			
-		
-			
+			}			
 		});
 		
+	
 		
 		}
 	
